94778bc7fe3f4eeaf173d10761e81d1ec9d09b5c
monemax <mone9732@gmail.com>
2024-05-21 18:43:51 +0500

    changes updated

--- src/components/Button/Starttrail.jsx
+++
@@ -1,9 +0,0 @@
-export default function(){
-    return(
-        <div className="freetrail">
-            <button>Start free trail</button>
-            <button className="get_demo">Get a demo</button>
-        </div>
-
-    )
-}
